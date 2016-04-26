5b2c20b546068a3aeca0718ac52f53466c71142d
W4pity <w4pity@gmail.com>
2016-04-26 10:26:20 +0100

    milestone 4: The graph is drawed

--- app/src/main/java/com/example/w4pity/gpstracer/MainActivity.java
+++ app/src/main/java/com/example/w4pity/gpstracer/MainActivity.java
@@ -158,6 +158,8 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
     }
 
 
+    
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
