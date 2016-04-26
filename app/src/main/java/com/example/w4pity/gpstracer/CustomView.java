877484a92a020f6d1e99ab6a040ab9edcf564c45
W4pity <w4pity@gmail.com>
2016-04-26 10:24:51 +0100

    milestone 3: The button work to activate or not the gps

--- app/src/main/java/com/example/w4pity/gpstracer/CustomView.java
+++ app/src/main/java/com/example/w4pity/gpstracer/CustomView.java
@@ -2,6 +2,13 @@ package com.example.w4pity.gpstracer;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
@@ -11,7 +18,16 @@ import android.view.View;
  */
 public class CustomView extends View {
 
-    public boolean started = false;
+    public boolean started = true;
+    public float[] tabSpeed = new float[100];
+    public int currentIndex = 0;
+    Paint white = new Paint(Paint.ANTI_ALIAS_FLAG);
+    Paint red = new Paint(Paint.ANTI_ALIAS_FLAG);
+    int height;
+    int width ;
+    static int  maxSpeedView = 6;
+
+
     public CustomView(Context c) {
         super(c);
 
@@ -21,8 +37,8 @@ public class CustomView extends View {
         super(c, as);
 
         init();
-
-
+        height = getWidth();
+        width =getHeight();
     }
 
     // constructor that take in a context, attribute set and also a default
@@ -35,12 +51,34 @@ public class CustomView extends View {
     // refactored init method as most of this code is shared by all the
 // constructors
     private void init() {
-
+        white.setColor(0xFFFFFFFF);
+        red.setColor(Color.RED);
     }
 
     public void onDraw(Canvas canvas) {
+
 // call the superclass method
         super.onDraw(canvas);
+
+        for(int i = 1; i<maxSpeedView; i++)
+        {
+
+            canvas.drawLine(0, i*getHeight()/maxSpeedView, getWidth(), i*getHeight()/maxSpeedView, white);
+        }
+        canvas.drawLine(0,getHeight()-(MainActivity.avSpeed/MainActivity.time*getHeight()/(maxSpeedView*10)),getWidth(),getHeight()-(MainActivity.avSpeed/MainActivity.time*getHeight()/(maxSpeedView*10)),red);
+
+
+        //canvas.drawRect(new Rect(0,0,10,10), white);
+        for(int i = 0; i<99; i++)
+        {
+            if(i == currentIndex-1)
+            canvas.drawRect( i * getWidth()/100, getHeight()-(tabSpeed[i] * getHeight() / (maxSpeedView*10)),i * getWidth()/100+10, getHeight()- (tabSpeed[i] * getHeight() /(maxSpeedView*10))+10, white);
+            //canvas.drawPoint(i * getWidth()/100, tabSpeed[i] * getHeight() / 60, white);
+            if(i != currentIndex-1 && i != currentIndex && i != currentIndex+1 && i!= currentIndex+2)
+            canvas.drawLine(i*getWidth()/100, getHeight()-(tabSpeed[i]*getHeight()/(maxSpeedView*10)), (i+1)*getWidth()/100, getHeight()-(tabSpeed[i+1]*getHeight()/(maxSpeedView*10)), white);
+          //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<!<!<<<<<<<<<<!<<<<< <!<<<<<< canvas.drawLine();
+        }
+        invalidate();
     }
 
 
--- app/src/main/java/com/example/w4pity/gpstracer/MainActivity.java
+++ app/src/main/java/com/example/w4pity/gpstracer/MainActivity.java
@@ -1,35 +1,162 @@
 package com.example.w4pity.gpstracer;
 
+import android.graphics.Color;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Display;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.text.DecimalFormat;
+
+public class MainActivity extends AppCompatActivity implements LocationListener {
+
+
+    LocationManager lm;
+    Location oldLoc;
+    static int time = 0;
+    static float avSpeed = 0;
+    static float dist = 0;
+
+    public float distance (float lat_a, float lng_a, float lat_b, float lng_b )
+    {
+        double earthRadius = 3958.75;
+        double latDiff = Math.toRadians(lat_b-lat_a);
+        double lngDiff = Math.toRadians(lng_b-lng_a);
+        double a = Math.sin(latDiff /2) * Math.sin(latDiff /2) +
+                Math.cos(Math.toRadians(lat_a)) * Math.cos(Math.toRadians(lat_b)) *
+                        Math.sin(lngDiff /2) * Math.sin(lngDiff /2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+        double distance = earthRadius * c;
+
+        int meterConversion = 1609;
+
+        return new Float(distance * meterConversion).floatValue();
+    }
+
+        @Override
+        public void onLocationChanged(Location location) {
+            DecimalFormat f = new DecimalFormat();
+            f.setMaximumFractionDigits(2);
+            CustomView cv = (CustomView) findViewById(R.id.Cv);
+            cv.tabSpeed[cv.currentIndex++] = (float)(location.getSpeed()*3.6);
+            if(cv.currentIndex == 99)
+                cv.currentIndex = 0;
+            //Toast.makeText(getApplicationContext(), "latitude: "+ location.getSpeed()*3.6, Toast.LENGTH_SHORT).show();
+            TextView t = (TextView) findViewById(R.id.time);
+            TextView s = (TextView) findViewById(R.id.speed);
+            TextView av = (TextView) findViewById(R.id.average);
+            TextView d = (TextView) findViewById(R.id.dist);
+            s.setText("Current speed: "+f.format(location.getSpeed()*3.6 )+" km/h");
+            avSpeed+=location.getSpeed()*3.6;
+            av.setText("Average speed :"+ f.format(avSpeed / time) + "km/h");
+            time++;
+            t.setText("Time: "+time+" s");
+            if(time>1)
+            {
+                //dist += (float) Math.sqrt(((location.getLatitude()-oldLoc.getLatitude())*(location.getLatitude()-oldLoc.getLatitude())+(location.getLongitude()-oldLoc.getLongitude())*(location.getLongitude()-oldLoc.getLongitude())));
+                //dist += 6371*Math.acos(Math.sin((location.getLatitude()/**Math.PI/180*/)*Math.sin(oldLoc.getLatitude()/**Math.PI/180*/)+Math.cos(location.getLatitude()/**Math.PI/180*/)*Math.cos((oldLoc.getLatitude()/* * Math.PI / 180*/) * Math.cos((location.getLongitude()/* * Math.PI / 180*/) -(oldLoc.getLongitude()/* * Math.PI / 180*/)))));
+                dist += distance((float)location.getLatitude(),(float) location.getLongitude(), (float)oldLoc.getLatitude(), (float)oldLoc.getLongitude())*0.001;
+                d.setText("Distance: "+f.format(dist)+" km");
+            }
+            oldLoc = location;
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        final CustomView cv = (CustomView) findViewById(R.id.Cv);
+        Display d = getWindowManager().getDefaultDisplay();
+        int width = d.getWidth();
+        cv.setLayoutParams(new LinearLayout.LayoutParams(width, width));
         final Button BStart = (Button) findViewById(R.id.start);
+        BStart.setBackgroundColor(Color.RED);
         BStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                CustomView cv = (CustomView) findViewById(R.id.Cv);
+
                 cv.started = !cv.started;
-                if(cv.started)
+                if (cv.started) {
                     BStart.setText("Stop Tracking");
-                else
+                    onResume();
+                    BStart.setBackgroundColor(Color.RED);
+                } else {
                     BStart.setText("Start Tracking");
+                    onPause();
+                    BStart.setBackgroundColor(Color.GREEN);
+                }
 
             }
         });
-
-
+        final Button valid = (Button) findViewById(R.id.valid);
+        valid.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText e = (EditText) findViewById(R.id.editText);
+                //e.setText("60");
+                cv.maxSpeedView = Integer.parseInt(e.getText().toString())/10;
+                Toast.makeText(getApplicationContext(),"Set to "+ cv.maxSpeedView*10+" km/h view", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        try {
+            lm.removeUpdates(this);
+        }
+        catch(Exception e){}
+    }
+    @Override
+    protected void onResume() {
+
+        super.onResume();
+
+        lm = (LocationManager) this.getSystemService(LOCATION_SERVICE);
+        TextView t = (TextView) findViewById(R.id.activation);
+
+
+        if (lm.isProviderEnabled(LocationManager.GPS_PROVIDER))
+            try {
+                lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0, this);
+                t.setText("Microchip: GPS activated");
+            }
+            catch(Exception e){}
+        else
+        try {
+            lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1000, 0, this);
+            t.setText("Microchip: Internet activated");
+        }
+        catch(Exception e){}
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
